2fa702b0d9b7278d602a4f3f131987c308359a94
lab319 <39893937+lab319@users.noreply.github.com>
2021-06-13 16:03:12 +0800

    Create fitnessn.m

---
+++ the proposed method/fitnessn.m
@@ -0,0 +1,10 @@
+function [y,SEAIQRD,pararout]=fitnessn(ParSwarm,data)
+N=data.N;
+x=data.intiad;
+t=1:data.step:data.maxday;
+[SEAIQRD,pararout]= Simulatea(ParSwarm,data.step,x,N,t,data);
+
+y=sum(sum(abs(data.China(1:data.maxday,[1 2 3 ])-SEAIQRD(:,[5 8 7 ])))./sum(abs(data.China(1:data.maxday,[1 2 3 ]))));
+
+
+end
